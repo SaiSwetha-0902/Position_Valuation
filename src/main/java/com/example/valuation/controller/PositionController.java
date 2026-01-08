cecd4f80df733e4d6f533364894e66d1c5f704fc
Samuela31 <msamuelaabigail@gmail.com>
2026-01-08 16:42:48 +0530

    Added CORS for frontend integration check

--- src/main/java/com/example/valuation/controller/PositionController.java
+++ src/main/java/com/example/valuation/controller/PositionController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/positions")
 public class PositionController {
--- src/main/java/com/example/valuation/controller/ValuationController.java
+++ src/main/java/com/example/valuation/controller/ValuationController.java
@@ -14,9 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/valuation")
+@RequestMapping("/api/valuation")
 public class ValuationController {
 	private static final Logger logger = LoggerFactory.getLogger(ValuationService.class);
 
